4d634ecb722b0c1168abb3da842515018d07597d
Aga Z <agataziemniak1@gmail.com>
2025-10-29 20:57:06 +0100

    chore: added tasks for tomorrow

---
+++ src/BoxingUnboxing/BoxingUnboxing.java
@@ -0,0 +1,9 @@
+package BoxingUnboxing;
+
+public class BoxingUnboxing {
+}
+
+//1.Perform both unboxing and boxing both manually and implicitly.
+//2.Perform both with autocasting.
+//3.perform explicit casting with manualual boxing/unboxing
+//4.perform explicit autoboxing/unboxing with manual casting.
---
+++ src/StaticMembers/StaticMembers.java
@@ -0,0 +1,26 @@
+package StaticMembers;
+
+//
+//public class StaticMembers {
+//}
+//tasks for 30.10
+
+//1.write a static variable in a class and modify its value. Figure out if its value changes only in further occurences.
+//2.rewrite to work properly:
+
+//public class MyClass{
+//    String hi = "Good Afternoon";
+//    public static void greet1(){System.out.println("Hello!");};
+//    private static void greet2(){System.out.println(System.out.println(hi));};
+//    private  void greet3(){System.out.println("Good day!");};
+//
+//    public static void greetAll(){
+//        greet1();
+//        greet2();
+//        greet3();
+//    }
+//}
+
+//3.Add a static import to this file.
+//write a static block
+
